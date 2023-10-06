e34c61fe11c31502e4b48b39afa5bc48045fc7ac
timre13 <itorteli13@gmail.com>
2023-10-06 13:16:07 +0200

    Work on navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<header>
+    <div id="header-left">
+        <a href="/">
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+                <!--
+                <path
+                    d="M448 80c8.8 0 16 7.2 16 16V415.8l-5-6.5-136-176c-4.5-5.9-11.6-9.3-19-9.3s-14.4 3.4-19 9.3L202 340.7l-30.5-42.7C167 291.7 159.8 288 152 288s-15 3.7-19.5 10.1l-80 112L48 416.3l0-.3V96c0-8.8 7.2-16 16-16H448zM64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"
+                /></svg
+            ></a
+        >
+    </div>
+
+    <div id="header-right">
+        <a href="/">About Me</a>
+        <a href="/">
+            <!--
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"
+                ><path
+                    d="M253.3 35.1c6.1-11.8 1.5-26.3-10.2-32.4s-26.3-1.5-32.4 10.2L117.6 192H32c-17.7 0-32 14.3-32 32s14.3 32 32 32L83.9 463.5C91 492 116.6 512 146 512H430c29.4 0 55-20 62.1-48.5L544 256c17.7 0 32-14.3 32-32s-14.3-32-32-32H458.4L365.3 12.9C359.2 1.2 344.7-3.4 332.9 2.7s-16.3 20.6-10.2 32.4L404.3 192H171.7L253.3 35.1zM192 304v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16zm96-16c8.8 0 16 7.2 16 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16zm128 16v96c0 8.8-7.2 16-16 16s-16-7.2-16-16V304c0-8.8 7.2-16 16-16s16 7.2 16 16z"
+                /></svg-->
+                >
+            </svg></a
+        >
+    </div>
+</header>
+<div id="content">
+    <slot />
+</div>
+
+<style lang="scss">
+    :global(*) {
+        margin: 0;
+        padding: 0;
+    }
+
+    header {
+        background-color: #d9d9d9;
+        height: 50px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+        a {
+            display: block;
+        }
+
+        #header-left,
+        #header-right {
+            display: flex;
+            flex-direction: row;
+        }
+
+        #header-right {
+            justify-content: right;
+        }
+
+        svg {
+            height: 100%;
+        }
+    }
+
+    #content {
+    }
+</style>
