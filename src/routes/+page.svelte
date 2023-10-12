af6c5e4b2120a0355ba44889b2959fb79844d214
timre13 <itorteli13@gmail.com>
2023-10-12 13:18:46 +0200

    Add welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="content-inner">
+    <div id="welcome">
+        <h1>Art Website</h1>
+        <h2>Lorem ipsum dolor sit amet</h2>
+    </div>
+</div>
+
+<style lang="scss">
+    #content-inner {
+        margin-top: -50px;
+        background-image: url(bg1.png);
+        background-size: cover;
+        min-height: 100vh;
+
+        #welcome {
+            padding-top: 30vh;
+            padding-left: 5rem;
+
+            h1 {
+                font-size: 60px;
+            }
+
+            h2 {
+                font-size: 40px;
+            }
+
+            h1,
+            h2 {
+                font-variant: small-caps;
+            }
+        }
+    }
+</style>
