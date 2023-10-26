1e9df9b995a0584111e0fc750a22abc7173801f5
vbjhbj <119402788+vbjhbj@users.noreply.github.com>
2023-10-26 12:46:56 +0200

    form

---
+++ src/routes/quote/+page.svelte
@@ -0,0 +1,25 @@
+<form action="POST">
+    <label>
+        First name: *
+        <input name="fname" type="text">
+    </label>
+    <label>
+        Last name: 
+        <input name="lname" type="text">
+    </label>
+    <label>
+		Email: *
+		<input name="email" type="email">
+	</label>
+    <label>
+        Phone number:
+        <input type="tel" name="phone">
+    </label>
+    <label>
+        Request: *
+        <input name="request" type="text">
+    </label>
+    <button type="submit">
+        Submit
+    </button>
+</form>
