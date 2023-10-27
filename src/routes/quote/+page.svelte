f935422f0e6ac6307b59f9c07dbc2be203988d02
vbjhbj <119402788+vbjhbj@users.noreply.github.com>
2023-10-27 09:32:27 +0200

    styling of quote page (except submit button)

--- src/routes/quote/+page.svelte
+++ src/routes/quote/+page.svelte
@@ -1,65 +1,75 @@
-<form action="POST">
-    <label>
-        First name: <span style="color: red;">*</span>
-        <br>
-        <input name="fname" type="text">
-    </label>
-    <br>
-    <label>
-        Last name: 
-        <br>
-        <input name="lname" type="text">
-    </label>
-    <br>
-    <label>
-		Email: <span style="color: red;">*</span>
-        <br>
-		<input name="email" type="email">
-	</label>
-    <br>
-    <label>
-        Phone number:
-        <br>
-        <input type="tel" name="phone">
-    </label>
-    <br>
-    <label>
-        I prefer contact by... <span style="color: red;">*</span>
-        <br>
-        <label class="radiob">
-            <input type="radio" name="contact" id="email">
-            E-mail
+<div>
+    <form action="POST">
+        <label>
+            First name: <span style="color: red;">*</span>
+            <br>
+            <input class="inpf" name="fname" type="text">
         </label>
-        <label class="radiob">
-            <input type="radio" name="contact" id="whatsapp">
-            Text(Whatsapp)
-        </label>
-        <label class="radiob">
-            <input type="radio" name="contact" id="other">
-            Other (in request)
-        </label>
-    </label>
-    <br>
-    <label>
-        Request: <span style="color: red;">*</span>
         <br>
-        <input name="request" type="text">
-    </label>
-    <br>
-    <button type="submit">
-        Submit
-    </button>
-</form>
-
+        <label>
+            Last name: 
+            <br>
+            <input class="inpf" name="lname" type="text">
+        </label>
+        <br>
+        <label>
+            Email: <span style="color: red;">*</span>
+            <br>
+            <input class="inpf" name="email" type="email">
+        </label>
+        <br>
+        <label>
+            Phone number:
+            <br>
+            <input class="inpf" type="tel" name="phone">
+        </label>
+        <br>
+        <label>
+            I prefer contact by... <span style="color: red;">*</span>
+            <br>
+            <label class="radiob">
+                <input type="radio" name="contact" id="email">
+                E-mail
+            </label>
+            <label class="radiob">
+                <input type="radio" name="contact" id="whatsapp">
+                Text(Whatsapp)
+            </label>
+            <label class="radiob">
+                <input type="radio" name="contact" id="other">
+                Other (in request)
+            </label>
+        </label>
+        <br>
+        <label>
+            Request: <span style="color: red;">*</span>
+            <br>
+            <input id="reqf" class="inpf" name="request" type="text">
+        </label>
+        <br>
+        <button type="submit">
+            Submit
+        </button>
+    </form>
+</div>
 <style>
     label{
         font-size: 24px;
     }
     .radiob{
-        font-size: 12px;
+        font-size: 14px;
     }
     form{
         margin-left: 67px;
         vertical-align: middle;
+        margin-top: 100px;
     }
+    .inpf{
+        height: 28px;
+        width: 330px;
+    }
+    #reqf{
+        height: 220px;
+    }
+
 </style>
