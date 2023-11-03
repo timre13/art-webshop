ee613c3d696198c52c2ac126cbf2147a75d799ae
vbjhbj <119402788+vbjhbj@users.noreply.github.com>
2023-11-03 13:39:56 +0100

    add intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div id="content-inner">
     <div id="welcome">
         <h1>Art Website</h1>
-        <h2>Lorem ipsum dolor sit amet</h2>
+        <h2>Hello my name is [NAME] and I am from <a href="https://www.openstreetmap.org/relation/1130390">[CITY]</a> , <a href="https://www.openstreetmap.org/relation/21335">[COUNTRY]</a>. I am [AGE] years old. I formally study(/ studied) at <a href="https://szbi-pg.hu">[ALMA MATER]</a>, from [YEAR] to [YEAR]. I'm most comfortable with <a href="">[ARTSTYLE]</a> and <a href="">[ARTSTYLE]</a>, and you can check out my work on <a href="facebook.com">[SOCIAL MEDIA]</a>, <a href="twitter.com">[SOCIAL MEDIA]</a> and <a href="instagram.com">[SOCIAL MEDIA]</a>.</h2>
     </div>
 </div>
 
