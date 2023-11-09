e125c96cef837246a537d2905578a07a83c2c2de
Törteli Imre <TorteliImre@pg.local>
2023-11-09 12:08:33 +0100

    New build

---
+++ docs/_app/immutable/chunks/paths.4c3494c8.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1e2icb5)==null?void 0:s.base)??"/art-webshop";var e;const t=((e=globalThis.__sveltekit_1e2icb5)==null?void 0:e.assets)??a;export{t as a,a as b};
